3041aea5ba782255f64f522476f3ac928d617207
faisalakb <faisalakb786@gmail.com>
2023-10-06 12:32:43 +0500

    Add enumerable logic

---
+++ MyEnumerable.rb
@@ -0,0 +1,17 @@
+module MyEnumerable
+    def all?(&block)
+        each { |item| return false unless yield(item) }
+        true
+    end
+
+    def any?(&block)
+        each { |item| return true if yield(item) }
+        false
+    end
+
+    def filter(&block)
+        resArray = []
+        each { |item| resArray << item if yield(item) }
+        resArray
+    end
+end
